ded877a6d2c77b55f9e1db4144b67fcd5eabc6b4
Zoltan Nemeth <zoltan.nemeth@schibsted.com>
2018-10-05 09:08:50 +0200

    2. commit
    Azt próbálom ki hogy hogyan tudok repoból csomag fájlokat 
    behívatkozni....

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/e-planet/pckg/stringutil"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(stringutil.Reverse("!ooooG ,olleH"))
 }
